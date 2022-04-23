9011759154916d7034c2040b9f2676355dffc6ae
David Solano <solanoed@uninorte.edu.co>
2022-04-23 00:38:26 -0500

    first commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager Instance;
+    [SerializeField] private Cell CellPrefab;
+    [SerializeField] private Player PlayerPrefab;
+    private Grid grid;
+    private Player player;
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        grid = new Grid(10, 10, 1, CellPrefab);
+
+        player = Instantiate(PlayerPrefab, new Vector2(0, 0), Quaternion.identity);  
+    }
+
+    public void CellMouseClick(int x, int y)
+    {
+        List<Cell> path = PathManager.Instance.FindPath(grid, (int)player.GetPosition.x, (int)player.GetPosition.y, x, y);
+
+        player.SetPath(path);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Path Finder
+
+![pathFinder](https://user-images.githubusercontent.com/4458129/161363964-f73e76d4-3763-4a82-ad93-ffc2ade5f7a2.gif)
